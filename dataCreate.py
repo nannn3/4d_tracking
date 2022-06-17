da06d9b1874d29d23556c965075ee847b9a15303
Tristen <millert9999@gmail.com>
2022-06-17 08:41:52 -0700

    Made the data matrix varible sized based on user input

--- dataCreate.py
+++ dataCreate.py
@@ -1,11 +1,32 @@
-f = open("data.txt",'w')
-#create an XYZ matrix:
-f.write('[')
-for z in range(10):
-    for y in range(10):
-        for x in range(10):
-            f.write(str(x)+","+str(y)+","+str(z))
-            f.write('\n')
-f.write(']')    
+"""
+File: dataCreate.py
+Author: Tristen Miller
+generates a 'data.txt' file with an XYZ matrix
+"""
+def createData(x,y,z):
+    print(x,y,z)
+    print(range(x))
+    f = open("data.txt",'w')
+    #create an XYZ matrix:
+    f.write('[')
+    for i in range(z+1):
+        for j in range(y+1):
+            for k in range(x+1):
+                f.write(str(k)+","+str(j)+","+str(i))
+                if i==z and j==y and k==x:
+                    break
+                f.write('\n')
+    f.write(']')    
 
-f.close()
+    f.close()
+    return
+
+def main():
+    x,y,z =input("Enter X Y Z dimensions ").split()
+    x=int(x)
+    y=int(y)
+    z=int(z)
+    createData(x,y,z)
+
+if __name__ == "__main__":
+    main()
